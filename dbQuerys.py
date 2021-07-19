ab854c2ae221b3dc56cc185c1748bfefefe54a5d
Jonas Klock <jonas.klock@hotmail.de>
2021-07-19 11:21:38 +0200

    added import functionality for all relevant tables

--- dbQuerys.py
+++ dbQuerys.py
@@ -1,5 +1,5 @@
-import time
-from datetime import datetime, timedelta
+# import time
+# from datetime import datetime, timedelta
 
 from DB import db
 
@@ -10,7 +10,44 @@ class dbQuerys:
 
         cursor = db.connection.cursor()
 
-        sql = """INSERT INTO Users (cardID, username, email, prename, surname, birthDate, sex, validityDate, state, note, rights, street, postcode, city)
-                 VALUES ('{cardID}', '{username}', '{email}', '{prename}', '{surname}', '{birthDate}', '{sex}', '{validityDate}', '{state}', '{note}', '{rights}', '{street}', {postcode}, '{city}')""".format(cardID=cardID, username=username, email=email, prename=prename, surname=surname, birthDate=birthDate, sex=sex, validityDate=validityDate, state=state, note=note, rights=rights, street=street, postcode=postcode, city=city)
+        sql = (
+            "INSERT INTO Users (cardID, username, email, prename, surname, birthDate, sex, validityDate, state, note, rights, street, postcode, city)"
+            f"VALUES ('{cardID}', '{username}', '{email}', '{prename}', '{surname}', '{birthDate}', '{sex}', '{validityDate}', '{state}', '{note}', '{rights}', '{street}', {postcode}, '{city}')"
+        )
+        cursor.execute(sql)
 
+    @classmethod
+    def addStrike(self, strikeID, sessionID, description):
+        db.connection.ping(reconnect = True)
+
+        cursor = db.connection.cursor()
+
+        sql = (
+            "INSERT INTO Strikes (strikeID, sessionID, description)"
+            f"VALUES ('{strikeID}', '{sessionID}', '{description}')"
+        )
+        cursor.execute(sql)
+
+    @classmethod
+    def addLog(self, sessionID, cardID, checkIn, checkOut):
+        db.connection.ping(reconnect = True)
+
+        cursor = db.connection.cursor()
+
+        sql = (
+            "INSERT INTO Log_Users (sessionID, cardID, checkIn, checkOut)"
+            f"VALUES ('{sessionID}', '{cardID}', '{checkIn}', '{checkOut}')"
+        )
+        cursor.execute(sql)
+
+    @classmethod
+    def addContract(self, contractID, email, contractBegin, contractEnd, contractPath, contractPaid):
+        db.connection.ping(reconnect = True)
+
+        cursor = db.connection.cursor()
+
+        sql = (
+            "INSERT INTO Contracts (contractID, email, contractBegin, contractEnd, contractPath, contractPaid)"
+            f"VALUES ('{contractID}', '{email}', '{contractBegin}', '{contractEnd}', '{contractPath}', '{contractPaid}')"
+        )
         cursor.execute(sql)
---
+++ import.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from dbQuerys import dbQuerys
+
+# main_dir = "IODB/"
+main_dir = ""
+
+df = pd.read_csv(f"{main_dir}csvs/users.csv", sep=",")
+
+def import_users():
+    with open(f"{main_dir}csvs/users.csv", "r") as src:
+        i = 0
+        for line in src.read().split("\n"):
+            if i:
+                line = line.split(",")
+                dbQuerys.addUser(
+                    line[0],
+                    line[1],
+                    line[2],
+                    line[3],
+                    line[4],
+                    line[5],
+                    line[6],
+                    line[7],
+                    line[8],
+                    line[9],
+                    line[10],
+                    line[11],
+                    line[12],
+                    line[13]
+                )
+                print(f"imported {line[0]}")
+            i += 1
+
+def import_strikes():
+    with open(f"{main_dir}csvs/strikes.csv", "r") as src:
+        i = 0
+        for line in src.read().split("\n"):
+            if i:
+                line = line.split(",")
+                dbQuerys.addStrike(
+                    line[0],
+                    line[1],
+                    line[2]
+                )
+                print(f"imported strike with strikeID {line[0]}")
+            i += 1
+            
+def import_log_users():
+    with open(f"{main_dir}csvs/log_users.csv", "r") as src:
+        i = 0
+        for line in src.read().split("\n"):
+            if i:
+                line = line.split(",")
+                dbQuerys.addLog(
+                    line[0],
+                    line[1],
+                    line[2],
+                    line[3]
+                )
+                print(f"imported log with sessionID {line[0]}")
+            i += 1
+            
+def import_contracts():
+    with open(f"{main_dir}csvs/contracts.csv", "r") as src:
+        i = 0
+        for line in src.read().split("\n"):
+            if i:
+                line = line.split(",")
+                dbQuerys.addContract(
+                    line[0],
+                    line[1],
+                    line[2],
+                    line[3],
+                    line[4],
+                    line[5]
+                )
+                print(f"imported contract with contractID {line[0]}")
+            i += 1
+
+import_users()
